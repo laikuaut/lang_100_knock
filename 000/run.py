f6fec6e0708925219cf89f4a3c33b5c754ecb4a8
shota <shota@localhost.localdomain>
2016-11-13 23:34:26 +0900

    00. 文字列の逆順

---
+++ 000/run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# config: utf-8
+
+def main():
+    input_str = 'stressed'
+    output_str = input_str[::-1]
+
+    print('input = {}'.format(input_str))
+    print('output = {}'.format(output_str))
+
+if __name__ == '__main__':
+    main()
