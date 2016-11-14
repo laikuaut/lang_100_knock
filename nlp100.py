559a5182a2e116f7297f33db8d79aa194eff32f7
shota <shota@localhost.localdomain>
2016-11-14 22:00:19 +0900

    ベースコミット

---
+++ nlp100.py
@@ -0,0 +1,405 @@
+#!/usr/bin/env python
+# config: utf-8
+
+class NLP_100():
+
+    @classmethod
+    def Q_000(cls):
+        pass
+
+    @classmethod
+    def Q_001(cls):
+        pass
+
+    @classmethod
+    def Q_002(cls):
+        pass
+
+    @classmethod
+    def Q_003(cls):
+        pass
+
+    @classmethod
+    def Q_004(cls):
+        pass
+
+    @classmethod
+    def Q_005(cls):
+        pass
+
+    @classmethod
+    def Q_006(cls):
+        pass
+
+    @classmethod
+    def Q_007(cls):
+        pass
+
+    @classmethod
+    def Q_008(cls):
+        pass
+
+    @classmethod
+    def Q_009(cls):
+        pass
+
+    @classmethod
+    def Q_010(cls):
+        pass
+
+    @classmethod
+    def Q_011(cls):
+        pass
+
+    @classmethod
+    def Q_012(cls):
+        pass
+
+    @classmethod
+    def Q_013(cls):
+        pass
+
+    @classmethod
+    def Q_014(cls):
+        pass
+
+    @classmethod
+    def Q_015(cls):
+        pass
+
+    @classmethod
+    def Q_016(cls):
+        pass
+
+    @classmethod
+    def Q_017(cls):
+        pass
+
+    @classmethod
+    def Q_018(cls):
+        pass
+
+    @classmethod
+    def Q_019(cls):
+        pass
+
+    @classmethod
+    def Q_020(cls):
+        pass
+
+    @classmethod
+    def Q_021(cls):
+        pass
+
+    @classmethod
+    def Q_022(cls):
+        pass
+
+    @classmethod
+    def Q_023(cls):
+        pass
+
+    @classmethod
+    def Q_024(cls):
+        pass
+
+    @classmethod
+    def Q_025(cls):
+        pass
+
+    @classmethod
+    def Q_026(cls):
+        pass
+
+    @classmethod
+    def Q_027(cls):
+        pass
+
+    @classmethod
+    def Q_028(cls):
+        pass
+
+    @classmethod
+    def Q_029(cls):
+        pass
+
+    @classmethod
+    def Q_030(cls):
+        pass
+
+    @classmethod
+    def Q_031(cls):
+        pass
+
+    @classmethod
+    def Q_032(cls):
+        pass
+
+    @classmethod
+    def Q_033(cls):
+        pass
+
+    @classmethod
+    def Q_034(cls):
+        pass
+
+    @classmethod
+    def Q_035(cls):
+        pass
+
+    @classmethod
+    def Q_036(cls):
+        pass
+
+    @classmethod
+    def Q_037(cls):
+        pass
+
+    @classmethod
+    def Q_038(cls):
+        pass
+
+    @classmethod
+    def Q_039(cls):
+        pass
+
+    @classmethod
+    def Q_040(cls):
+        pass
+
+    @classmethod
+    def Q_041(cls):
+        pass
+
+    @classmethod
+    def Q_042(cls):
+        pass
+
+    @classmethod
+    def Q_043(cls):
+        pass
+
+    @classmethod
+    def Q_044(cls):
+        pass
+
+    @classmethod
+    def Q_045(cls):
+        pass
+
+    @classmethod
+    def Q_046(cls):
+        pass
+
+    @classmethod
+    def Q_047(cls):
+        pass
+
+    @classmethod
+    def Q_048(cls):
+        pass
+
+    @classmethod
+    def Q_049(cls):
+        pass
+
+    @classmethod
+    def Q_050(cls):
+        pass
+
+    @classmethod
+    def Q_051(cls):
+        pass
+
+    @classmethod
+    def Q_052(cls):
+        pass
+
+    @classmethod
+    def Q_053(cls):
+        pass
+
+    @classmethod
+    def Q_054(cls):
+        pass
+
+    @classmethod
+    def Q_055(cls):
+        pass
+
+    @classmethod
+    def Q_056(cls):
+        pass
+
+    @classmethod
+    def Q_057(cls):
+        pass
+
+    @classmethod
+    def Q_058(cls):
+        pass
+
+    @classmethod
+    def Q_059(cls):
+        pass
+
+    @classmethod
+    def Q_060(cls):
+        pass
+
+    @classmethod
+    def Q_061(cls):
+        pass
+
+    @classmethod
+    def Q_062(cls):
+        pass
+
+    @classmethod
+    def Q_063(cls):
+        pass
+
+    @classmethod
+    def Q_064(cls):
+        pass
+
+    @classmethod
+    def Q_065(cls):
+        pass
+
+    @classmethod
+    def Q_066(cls):
+        pass
+
+    @classmethod
+    def Q_067(cls):
+        pass
+
+    @classmethod
+    def Q_068(cls):
+        pass
+
+    @classmethod
+    def Q_069(cls):
+        pass
+
+    @classmethod
+    def Q_070(cls):
+        pass
+
+    @classmethod
+    def Q_071(cls):
+        pass
+
+    @classmethod
+    def Q_072(cls):
+        pass
+
+    @classmethod
+    def Q_073(cls):
+        pass
+
+    @classmethod
+    def Q_074(cls):
+        pass
+
+    @classmethod
+    def Q_075(cls):
+        pass
+
+    @classmethod
+    def Q_076(cls):
+        pass
+
+    @classmethod
+    def Q_077(cls):
+        pass
+
+    @classmethod
+    def Q_078(cls):
+        pass
+
+    @classmethod
+    def Q_079(cls):
+        pass
+
+    @classmethod
+    def Q_080(cls):
+        pass
+
+    @classmethod
+    def Q_081(cls):
+        pass
+
+    @classmethod
+    def Q_082(cls):
+        pass
+
+    @classmethod
+    def Q_083(cls):
+        pass
+
+    @classmethod
+    def Q_084(cls):
+        pass
+
+    @classmethod
+    def Q_085(cls):
+        pass
+
+    @classmethod
+    def Q_086(cls):
+        pass
+
+    @classmethod
+    def Q_087(cls):
+        pass
+
+    @classmethod
+    def Q_088(cls):
+        pass
+
+    @classmethod
+    def Q_089(cls):
+        pass
+
+    @classmethod
+    def Q_090(cls):
+        pass
+
+    @classmethod
+    def Q_091(cls):
+        pass
+
+    @classmethod
+    def Q_092(cls):
+        pass
+
+    @classmethod
+    def Q_093(cls):
+        pass
+
+    @classmethod
+    def Q_094(cls):
+        pass
+
+    @classmethod
+    def Q_095(cls):
+        pass
+
+    @classmethod
+    def Q_096(cls):
+        pass
+
+    @classmethod
+    def Q_097(cls):
+        pass
+
+    @classmethod
+    def Q_098(cls):
+        pass
+
+    @classmethod
+    def Q_099(cls):
+        pass
+
---
+++ test/test_nlp100.py
@@ -0,0 +1,310 @@
+#!/usr/bin/env python
+# config: utf-8
+
+import unittest
+from nlp100 import NLP_100
+
+class Test_NLP_100(unittest.TestCase):
+
+    def test_Q_000(self):
+        pass
+
+    def test_Q_001(self):
+        pass
+
+    def test_Q_002(self):
+        pass
+
+    def test_Q_003(self):
+        pass
+
+    def test_Q_004(self):
+        pass
+
+    def test_Q_005(self):
+        pass
+
+    def test_Q_006(self):
+        pass
+
+    def test_Q_007(self):
+        pass
+
+    def test_Q_008(self):
+        pass
+
+    def test_Q_009(self):
+        pass
+
+    def test_Q_010(self):
+        pass
+
+    def test_Q_011(self):
+        pass
+
+    def test_Q_012(self):
+        pass
+
+    def test_Q_013(self):
+        pass
+
+    def test_Q_014(self):
+        pass
+
+    def test_Q_015(self):
+        pass
+
+    def test_Q_016(self):
+        pass
+
+    def test_Q_017(self):
+        pass
+
+    def test_Q_018(self):
+        pass
+
+    def test_Q_019(self):
+        pass
+
+    def test_Q_020(self):
+        pass
+
+    def test_Q_021(self):
+        pass
+
+    def test_Q_022(self):
+        pass
+
+    def test_Q_023(self):
+        pass
+
+    def test_Q_024(self):
+        pass
+
+    def test_Q_025(self):
+        pass
+
+    def test_Q_026(self):
+        pass
+
+    def test_Q_027(self):
+        pass
+
+    def test_Q_028(self):
+        pass
+
+    def test_Q_029(self):
+        pass
+
+    def test_Q_030(self):
+        pass
+
+    def test_Q_031(self):
+        pass
+
+    def test_Q_032(self):
+        pass
+
+    def test_Q_033(self):
+        pass
+
+    def test_Q_034(self):
+        pass
+
+    def test_Q_035(self):
+        pass
+
+    def test_Q_036(self):
+        pass
+
+    def test_Q_037(self):
+        pass
+
+    def test_Q_038(self):
+        pass
+
+    def test_Q_039(self):
+        pass
+
+    def test_Q_040(self):
+        pass
+
+    def test_Q_041(self):
+        pass
+
+    def test_Q_042(self):
+        pass
+
+    def test_Q_043(self):
+        pass
+
+    def test_Q_044(self):
+        pass
+
+    def test_Q_045(self):
+        pass
+
+    def test_Q_046(self):
+        pass
+
+    def test_Q_047(self):
+        pass
+
+    def test_Q_048(self):
+        pass
+
+    def test_Q_049(self):
+        pass
+
+    def test_Q_050(self):
+        pass
+
+    def test_Q_051(self):
+        pass
+
+    def test_Q_052(self):
+        pass
+
+    def test_Q_053(self):
+        pass
+
+    def test_Q_054(self):
+        pass
+
+    def test_Q_055(self):
+        pass
+
+    def test_Q_056(self):
+        pass
+
+    def test_Q_057(self):
+        pass
+
+    def test_Q_058(self):
+        pass
+
+    def test_Q_059(self):
+        pass
+
+    def test_Q_060(self):
+        pass
+
+    def test_Q_061(self):
+        pass
+
+    def test_Q_062(self):
+        pass
+
+    def test_Q_063(self):
+        pass
+
+    def test_Q_064(self):
+        pass
+
+    def test_Q_065(self):
+        pass
+
+    def test_Q_066(self):
+        pass
+
+    def test_Q_067(self):
+        pass
+
+    def test_Q_068(self):
+        pass
+
+    def test_Q_069(self):
+        pass
+
+    def test_Q_070(self):
+        pass
+
+    def test_Q_071(self):
+        pass
+
+    def test_Q_072(self):
+        pass
+
+    def test_Q_073(self):
+        pass
+
+    def test_Q_074(self):
+        pass
+
+    def test_Q_075(self):
+        pass
+
+    def test_Q_076(self):
+        pass
+
+    def test_Q_077(self):
+        pass
+
+    def test_Q_078(self):
+        pass
+
+    def test_Q_079(self):
+        pass
+
+    def test_Q_080(self):
+        pass
+
+    def test_Q_081(self):
+        pass
+
+    def test_Q_082(self):
+        pass
+
+    def test_Q_083(self):
+        pass
+
+    def test_Q_084(self):
+        pass
+
+    def test_Q_085(self):
+        pass
+
+    def test_Q_086(self):
+        pass
+
+    def test_Q_087(self):
+        pass
+
+    def test_Q_088(self):
+        pass
+
+    def test_Q_089(self):
+        pass
+
+    def test_Q_090(self):
+        pass
+
+    def test_Q_091(self):
+        pass
+
+    def test_Q_092(self):
+        pass
+
+    def test_Q_093(self):
+        pass
+
+    def test_Q_094(self):
+        pass
+
+    def test_Q_095(self):
+        pass
+
+    def test_Q_096(self):
+        pass
+
+    def test_Q_097(self):
+        pass
+
+    def test_Q_098(self):
+        pass
+
+    def test_Q_099(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
