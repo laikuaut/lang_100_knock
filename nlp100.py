71d6ff171f86b438a34469dba7bebf711f12904e
shota <shota@localhost.localdomain>
2016-11-14 23:35:24 +0900

    03,04追加

--- nlp100.py
+++ nlp100.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # config: utf-8
 
+import re
+
 class NLP_100():
 
     @classmethod
@@ -43,11 +45,38 @@ class NLP_100():
 
     @classmethod
     def Q_003(cls):
-        pass
+        """ 03. 円周率
+        "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+        という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+        """
+
+        # 単語の文字数が円周率=3.14159265358979になっている
+        input_str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+        word_list = re.split(r'\W+', input_str)
+        return [len(word) for word in word_list if len(word) > 0]
 
     @classmethod
     def Q_004(cls):
-        pass
+        """ 04. 元素記号
+        "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+        という文を単語に分解し，1, 5, 6, 7, 8, 9, 15, 16, 19番目の単語は先頭の1文字，それ以外の単語は先頭に2文字を取り出し，
+        取り出した文字列から単語の位置（先頭から何番目の単語か）への連想配列（辞書型もしくはマップ型）を作成せよ．
+        """
+
+        # 結果がkey=元素記号, value=元素番号の辞書型になる
+        input_str = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+        word_list = [word for word in re.split(r'\W+', input_str) if len(word) > 0]
+        one_pos_list = [1, 5, 6, 7, 8, 9, 15, 16, 19]
+
+        # 元素記号格納辞書
+        element = {}
+        for num, word in enumerate(word_list):
+            if num+1 in one_pos_list:
+                element[word[0:1]] = num+1
+            else:
+                element[word[0:2]] = num+1
+
+        return element
 
     @classmethod
     def Q_005(cls):
--- test/test_nlp100.py
+++ test/test_nlp100.py
@@ -16,9 +16,15 @@ class Test_NLP_100(unittest.TestCase):
         self.assertEqual(NLP_100.Q_002(), 'パタトクカシーー')
 
     def test_Q_003(self):
-        pass
+        self.assertEqual(NLP_100.Q_003(), \
+            [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9])
 
     def test_Q_004(self):
+        self.assertEqual(NLP_100.Q_004(), \
+            {'H':1,'He':2,'Li':3,'Be':4,'B':5,'C':6,'N':7,
+             'O':8,'F':9,'Ne':10,'Na':11,'Mi':12,'Al':13,
+             'Si':14,'P':15,'S':16,'Cl':17,'Ar':18,'K':19,
+             'Ca':20})
         pass
 
     def test_Q_005(self):
