b2189a13e01eee79ec7a497a50631a785056a7c1
laikuaut <laikuaut@gmail.com>
2016-11-29 00:04:55 +0900

    Q23コメント追加

--- nlp100/Q023.py
+++ nlp100/Q023.py
@@ -3,16 +3,35 @@
 import re
 
 class Section(object):
+    """ セクション構造クラス
+    セクション構造の行を読み込み保存する。
+    """
     def __init__(self):
+        """ 初期化
+        メンバ変数を初期化
+        """
+
+        # セクション構造行抽出用正規表現
         self.__re_secrion_line = re.compile(r'^==+.+==+$')
+        # セクション構造のレベル部分文字列抽出正規表現
         self.__re_section_level = re.compile(r'==+')
+        # (セクション名, セクションレベル)のタプル格納リスト
         self.section_list = []
 
     def addSection(self, line):
+        """ セクション行追加
+        入力行がセクション構造の行であるかを判定し、
+        セクション行である場合は、セクション情報リストへ追加する。
+        """
         if self.__re_secrion_line.match(line):
-            self.section_list.append(self.getSection(line))
+            self.section_list.append(self.__getSection(line))
 
-    def getSection(self, line):
+    def __getSection(self, line):
+        """ セクション情報タプル取得
+        セクション行を解析して(セクション名, セクションレベル)のタプルを返却する。
+        ※ 入力行はaddSectionにてセクション行であるかを判定済みの前提
+        """
+
         level_symbol = self.__re_section_level.match(line).group()
         level = len(level_symbol) - 1
         name = line.replace(level_symbol, '')
